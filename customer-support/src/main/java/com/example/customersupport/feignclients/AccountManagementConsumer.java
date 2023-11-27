743a6f22698a894e84d78364198caac5173dc861
Pridemore <pridemoreviriri@gmail.com>
2023-11-27 20:03:06 +0200

    Updated account consumer to use api gateway

--- customer-support/src/main/java/com/example/customersupport/feignclients/AccountManagementConsumer.java
+++ customer-support/src/main/java/com/example/customersupport/feignclients/AccountManagementConsumer.java
@@ -5,8 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("ACCOUNT-MANAGEMENT-SERVICE")
+@FeignClient("api-gateway")
 public interface AccountManagementConsumer {
-    @GetMapping("/api/v1/account/getUserDetailsByAccount/{accountNumber}")
+    @GetMapping("/account-management-service/api/v1/account/getUserDetailsByAccount/{accountNumber}")
     CommonResponse getUserDetailsByAccountNumber(@PathVariable("accountNumber") String accountNumber);
 }
