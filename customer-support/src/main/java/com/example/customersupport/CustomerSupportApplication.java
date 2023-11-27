ab0ce588b68c7925ed61790a9ffa4653bcdc99a5
Pridemore <pridemoreviriri@gmail.com>
2023-11-27 19:14:42 +0200

    updated dependences for customer support service

--- customer-support/src/main/java/com/example/customersupport/CustomerSupportApplication.java
+++ customer-support/src/main/java/com/example/customersupport/CustomerSupportApplication.java
@@ -3,9 +3,11 @@ package com.example.customersupport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableFeignClients
 public class CustomerSupportApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerSupportApplication.class, args);
