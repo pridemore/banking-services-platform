d69976fa1db03fa7b728950f6db5d6f9994f20a8
Pridemore <pridemoreviriri@gmail.com>
2023-11-27 20:32:18 +0200

    Added security Bearer token through API gateway

--- api-gateway/src/main/java/com/example/apigateway/config/CustormPreFilter.java
+++ api-gateway/src/main/java/com/example/apigateway/config/CustormPreFilter.java
@@ -1,9 +1,13 @@
 package com.example.apigateway.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
@@ -13,18 +17,38 @@ import reactor.core.publisher.Mono;
 @Slf4j
 public class CustormPreFilter implements GlobalFilter {
 
+    @Value("${api.authorization.key}")
+    private String apiKey;
+
     public Mono<Void> filter(final ServerWebExchange exchange, GatewayFilterChain chain) {
         log.info("Pre filter called here :-----------");
         ServerHttpRequest request = exchange.getRequest();
-        log.info("Authorization = "+ request.getHeaders().getFirst("Authorization"));
+        log.info("Authorization = " + request.getHeaders().getFirst("Authorization"));
 
-        //rerouting to whatever the correct service
-        //return chain.filter(exchange);
+        if (!hasValidAuthorization(request)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            exchange.getResponse().getHeaders().setContentType(MediaType.APPLICATION_JSON);
+            String errorMessage = "{\"error\":\"Unauthorized\"}";
+            byte[] errorMessageBytes = errorMessage.getBytes();
+            return exchange.getResponse().writeWith(Mono.just(exchange.getResponse().bufferFactory().wrap(errorMessageBytes)));
+        }
 
-        //post filter and prefilter re-routing;
-        return  chain.filter(exchange).then(Mono.<Void>fromRunnable(()->{
-            ServerHttpResponse response=exchange.getResponse();
-            log.info("Post filter = "+response.getStatusCode());
+        return chain.filter(exchange).then(Mono.<Void>fromRunnable(() -> {
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("Post filter = " + response.getStatusCode());
         }));
     }
+
+    private boolean hasValidAuthorization(ServerHttpRequest request) {
+        HttpHeaders headers = request.getHeaders();
+        String authorizationHeader = headers.getFirst("Authorization");
+
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")
+                && request.getHeaders().getFirst("Authorization")
+                .substring(7).equals(apiKey)) {
+            return true;
+        }
+        return false;
+    }
+
 }
